3957ed6d9c2af543ec48a6cbae71c13888b72b49
Joshua Sibanda <75424803+joshua-inf@users.noreply.github.com>
2022-09-24 00:45:40 +0200

    addd socials

--- src/body/About.js
+++ src/body/About.js
@@ -1,12 +1,34 @@
 import './body.css'
+import { AiFillGithub } from 'react-icons/ai'
+import { AiOutlineFacebook } from 'react-icons/ai'
+import { FiInstagram } from 'react-icons/fi'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { BsTwitch } from 'react-icons/bs'
 
-const About = () => { 
+const About = () => {
     return <div className="content black">
         <div className="A-cont">
             <h2>Joshua Sibanda</h2>
             <h4>Web Developer</h4>
             <hr />
-            <h5>skills</h5>
+            <h5>Socials</h5>
+            <div className="social-icons">
+                <a href="http://">
+                    <BsTwitch />
+                </a>
+                <a href="http://">
+                    <AiOutlineTwitter />
+                </a>
+                <a href="http://">
+                    <FiInstagram />
+                </a>
+                <a href="http://">
+                    <AiOutlineFacebook />
+                </a>
+                <a href="https://github.com/joshua-inf">
+                    <AiFillGithub />
+                </a>
+            </div>
         </div>
     </div>
 }
