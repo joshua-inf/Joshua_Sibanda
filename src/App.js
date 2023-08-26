5dca6ece31dec1f199842adba32f057d88b97070
joshua-inf <jsibanda407@gmial.com>
2023-08-27 00:25:34 +0200

    done with the suspense

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ import image from './img/3d-render-abstract-particle-design.jpg'
 import Nav from './component/nav.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Particlesbg from './body/Particles';
+import { Suspense } from 'react';
 
 function App() {
+  const Loading = () => {
+    return (
+      <>
+        <div>
+          <div className='fs-1'>Loading...</div>
+        </div>
+      </>
+    )
+  }
   return (
     <>
-    <div className="App "  style={{minHeight:'100vh'}}>
-    <Particlesbg/>
-    <BrowserRouter>
-    <div style={{zIndex:'2'}}>
-        <Nav/>
-        <Routes>
-          <Route path='/Joshua_Sibanda/' element={<Body/>}/>
-        </Routes>
-    </div>
-    </BrowserRouter>
-    </div>
+      <div className="App " style={{ minHeight: '100vh' }}>
+        <Particlesbg />
+        <BrowserRouter>
+          <div style={{ zIndex: '2' }}>
+            <Nav />
+            <Routes>
+              <Route path='/Joshua_Sibanda/' element={
+                <Suspense fallback={<Loading />}>
+                  <Body />
+                </Suspense>
+              } />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
